e14e04b63e3a61426ce23c4e06a6a45d3752f023
palo-landrae <palo.loui.andrae@iisgalvanimi.edu.it>
2021-01-14 10:32:46 +0000

    punto 7

--- angular-post-spammer/src/app/app.module.ts
+++ angular-post-spammer/src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { SpamPrinterComponent } from './spam-printer/spam-printer.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
@@ -10,7 +11,8 @@ import { SpamPrinterComponent } from './spam-printer/spam-printer.component';
     SpamPrinterComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
